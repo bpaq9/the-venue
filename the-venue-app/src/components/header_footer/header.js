9ab55867236e2e2285c17bd1be42718ce0b7e102
Ben Paquin <bpaquin9@gmail.com>
2019-01-03 10:19:34 -0500

    Implementing AppBar and Toolbar components
    This also adds text and padding to the components

--- the-venue-app/src/components/header_footer/header.js
+++ the-venue-app/src/components/header_footer/header.js
@@ -8,9 +8,22 @@ import IconButton from '@material-ui/core/IconButton';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <AppBar
+                position="fixed"
+                style={{
+                    backgroundColor: '#2f2f2f',
+                    boxShadow: 'none',
+                    padding: '10px 0'
+                }}
+            >
+                <Toolbar>
+
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">The Venue</div>
+                        <div className="header_logo_title">Musical Events</div>
+                    </div>
+                </Toolbar>
+            </AppBar>
         );
     }
 }
