d8b51c08f94f85f3befad874843ebf634bb78556
Ben Paquin <bpaquin9@gmail.com>
2019-01-14 20:47:21 -0500

    adding highlights section
    includes discount component, highlights component, and description component.
    
    Fixes to typos in css

--- the-venue-app/src/App.js
+++ the-venue-app/src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css'
 import Header from './components/header_footer/Header.js';
 import Featured from './components/Featured/';
 import VenueNFO from './components/venueNFO';
+import Highlights from './components/Highlights';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
+      <div className="App" style={{ height: "1500px"}}>
         <Header />
         <Featured />
         <VenueNFO />
+        <Highlights/>
       </div>
     );
   }
