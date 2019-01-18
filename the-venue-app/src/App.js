f2a04d22bda00c4706d0831ba586913bcddeac54
Ben Paquin <bpaquin9@gmail.com>
2019-01-17 19:12:40 -0500

    adds location and footer components and beginning scroller component

--- the-venue-app/src/App.js
+++ the-venue-app/src/App.js
@@ -6,16 +6,20 @@ import Featured from './components/Featured/';
 import VenueNFO from './components/venueNFO';
 import Highlights from './components/Highlights';
 import Pricing from './components/Pricing/';
+import Location from './components/Location';
+import Footer from './components/header_footer/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: "1500px"}}>
-        <Header />
-        <Featured />
-        <VenueNFO />
+        <Header/>
+        <Featured/>
+        <VenueNFO/>
         <Highlights/>
-        <Pricing />
+        <Pricing/>
+        <Location/>
+        <Footer/>
       </div>
     );
   }
---
+++ the-venue-app/src/components/Location/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Location = () => {
+    return (
+        <div className="location_wrapper">
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12618.779420223822!2d-122.2029607!3d37.7503024!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6497349cedd00de4!2sOracle+Arena!5e0!3m2!1sen!2sus!4v1547767773366" 
+                width="100%" 
+                height="500px" 
+                frameBorder="0" 
+                allowFullScreen
+                ></iframe>
+        
+        <div className="location_tag">
+            <div>Location</div>
+        </div>
+        </div>
+    );
+};
+
+export default Location;
